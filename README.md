31f09d7f2b4047b135fd2058919262eb5fe354cd
Suresh Mohan <sureforall@gmail.com>
2019-06-18 15:57:44 +0530

    Adding the project to the repository.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# convert-word-to-pdf
-The repository contains a Console Application to convert word document to PDF by using Syncfusion DocToPdfConvertor
+# Convert Word Document to PDF
+The repository contains a Console Application to convert word document to PDF by using [Syncfusion Word to PDF converter](https://www.syncfusion.com/word-framework/net/word-to-pdf-conversion)
+
+## How to run the project
+
+* Checkout this project to a location in your disk.
+* Open the solution file using the Visual Studio.
+* Run the project to install the required nuget packages and deploy the application.
---
+++ WordToPDF/Program.cs
@@ -0,0 +1,35 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using Syncfusion.DocToPDFConverter;
+using Syncfusion.Pdf;
+
+namespace WordToPDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get the path of existing Word document
+            string fullpath = @"...\..\DocToPDF.docx";
+
+            //Loads an existing Word document
+            WordDocument wordDocument = new WordDocument(fullpath, FormatType.Docx);
+            
+            //Creates an instance of the DocToPDFConverter
+            DocToPDFConverter converter = new DocToPDFConverter();
+
+            //Converts Word document into PDF document
+            PdfDocument pdfDocument = converter.ConvertToPDF(wordDocument);
+            
+            //Releases all resources used by DocToPDFConverter
+            converter.Dispose();
+
+            //Saves the PDF file 
+            pdfDocument.Save("DocToPDF.pdf");
+
+            //Closes the instance of document objects
+            pdfDocument.Close(true);
+            wordDocument.Close();
+        }
+    }
+}
